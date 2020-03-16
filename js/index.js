c9b7f307fa0e4ef1c42449d1a828c76aba6d47fb
YuriiMykhailiv <yuriimykhailiv@gmail.com>
2020-03-16 12:15:16 +0000

    Sample Code

--- js/index.js
+++ js/index.js
@@ -13,3 +13,18 @@ document.querySelectorAll('.opers button')
 function operPressed(ev) {
     display.value += ev.target.innerText;
 }
+
+document.querySelector('.reset').addEventListener('click', reset);
+     function reset() {
+         display.value = " ";         
+
+}
+document.querySelector('.eq').addEventListener('click', calc);
+     function calc() {
+         display.value = eval(display.value);        
+        }
+document.querySelector('.back').addEventListener('click', back);
+function back() {
+    var exp = display.value;
+    display.value = exp.substring(0, exp.length - 1);
+}
